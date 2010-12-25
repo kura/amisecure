25b326303c4919d7b25c4ced943c641924f2d168
Kura <kura@tangentlabs.co.uk>
2010-12-25 13:44:13 +0000

    Added a couple more checks, tidied up method names, cleaner output

--- amisecure.py
+++ amisecure.py
@@ -7,15 +7,16 @@ def write_to_shell(message, yes_no, status):
         colour = "\x1b[01;32m"
     else:
         colour = "\x1b[01;31m"
-    sys.stdout.write("%s ... " % (message))
+    sys.stdout.write("- %s ... " % (message))
     sys.stdout.write(colour + yes_no.upper() + "\x1b[00m" + "\n")
 
 class ssh(object):
 
     def __init__(self):
+        sys.stdout.write("Checking SSHD configuration\n")
         self.content = open("/etc/ssh/sshd_config", "r").read()
 
-    def check_root_logins(self):
+    def root_logins(self):
         if re.search("PermitRootLogin+\s+yes", self.content):
             value = "yes"
             good = False
@@ -25,7 +26,7 @@ class ssh(object):
 
         write_to_shell("Permit root logins", value, good)
 
-    def use_privilege_separation(self):
+    def privilege_separation(self):
         if re.search("UsePrivilegeSeparation+\s+yes", self.content):
             value = "yes"
             good = True
@@ -35,6 +36,30 @@ class ssh(object):
 
         write_to_shell("Use privilege separation", value, good)
 
+    def strict_modes(self):
+        if re.search("StrictModes+\s+yes", self.content):
+            value = "yes"
+            good = True
+        else:
+            value = "no"
+            good = False
+
+        write_to_shell("Use strict modes", value, good)
+
+    def permit_empty_passwords(self):
+        if re.search("PermitEmptyPasswords+\s+yes", self.content):
+            value = "yes"
+            good = False
+        else:
+            value = "no"
+            good = True
+
+        write_to_shell("Permit empty passwords", value, good)
+
+sys.stdout.write("Checking system security features\n\n")
+
 ssh = ssh()
-ssh.check_root_logins()
-ssh.use_privilege_separation()
+ssh.root_logins()
+ssh.privilege_separation()
+ssh.strict_modes()
+ssh.permit_empty_passwords()
