bda4125a6776bb1a08a728924e9a0db3ad088df7
Kura <kura@deviling.net>
2010-12-25 23:09:51 +0000

    Cleaned up the code. No need for dozens of classes

--- amisecure.py
+++ amisecure.py
@@ -2,93 +2,45 @@
 import re
 import sys
 
-def write_to_shell(message, yes_no, status):
+checks = (
+    {
+        "name": "ssh",
+        "file": "/etc/ssh/sshd_config",
+        "tests": (
+            ("PermitRootLogin+\s+(yes|no)", "no", "Permit root logins"),
+            ("UsePrivilegeSeparation+\s+(yes|no)", "yes", "Use privilege separation"),
+            ("StrictModes+\s+(yes|no)", "yes", "Use strict modes"),
+            ("PermitEmptyPasswords+\s+(yes|no)", "no", "Permit empty passwords"),
+        ),
+    },
+)
+
+def write_to_shell(message, value, status):
     u"""Output response to shell"""
     if status:
         colour = "\x1b[01;32m"
+        value = value + " (secure)"
     else:
         colour = "\x1b[01;31m"
+        value = value + " (not secure)"
     sys.stdout.write("- %s ... " % (message))
-    sys.stdout.write(colour + yes_no.upper() + "\x1b[00m" + "\n")
+    sys.stdout.write(colour + value.upper() + "\x1b[00m" + "\n")
 
-def check(regex, match_is_secure, message, content):
+def check(regex, secure_value, message, content):
     u"""Test method for doing entire check without code replication"""
     rx = re.compile(regex)
     if rx.search(content):
-        if match_is_secure:
+        value = rx.search(content).group(1)
+        if secure_value == value:
             secure = True
         else:
             secure = False
-    write_to_shell(message, rx.search(content).group(1), secure)
+    write_to_shell(message, value, secure)
 
-checks = {
-    "ssh": {
-        "file": "/etc/ssh/sshd_config",
-        "tests": (
-            ("PermitRootLogin+\s+(yes)", False, "Permit root logins"),
-            ("UsePrivilegeSeparation+\s+yes", True, "Use privilege separation"),
-            ("StrictModes+\s+yes", True, "Use strict modes"),
-            ("PermitEmptyPasswords+\s+yes", False, "Permit empty passwords"),
-        ),
-    },
-}
+for system in checks:
+    sys.stdout.write("Checking: %s\n" % (system['name']))
+    content = open(system['file'], "r").read()
+    for test in system['tests']:
+        check(test[0], test[1], test[2], content)
 
-
-class ssh(object):
-    u"""SSH security class"""
-
-    def __init__(self):
-        sys.stdout.write("Checking SSHD configuration\n")
-        self.content = open("/etc/ssh/sshd_config", "r").read()
-
-    def root_logins(self):
-        u"""Old basic test for root logins being enabled"""
-        if re.search("PermitRootLogin+\s+(yes)", self.content):
-            value = "yes"
-            good = False
-        else:
-            value = "no"
-            good = True
-
-        write_to_shell("Permit root logins", value, good)
-
-    def privilege_separation(self):
-        u"""Old basic test for privilege being enabled"""
-        if re.search("UsePrivilegeSeparation+\s+yes", self.content):
-            value = "yes"
-            good = True
-        else:
-            value = "no"
-            good = False
-
-        write_to_shell("Use privilege separation", value, good)
-
-    def strict_modes(self):
-        u"""Old basic test for strict modes being enabled"""
-        if re.search("StrictModes+\s+yes", self.content):
-            value = "yes"
-            good = True
-        else:
-            value = "no"
-            good = False
-
-        write_to_shell("Use strict modes", value, good)
-
-    def permit_empty_passwords(self):
-        u"""Old basic test for empty passwords being enabled"""
-        if re.search("PermitEmptyPasswords+\s+yes", self.content):
-            value = "yes"
-            good = False
-        else:
-            value = "no"
-            good = True
-
-        write_to_shell("Permit empty passwords", value, good)
-
-sys.stdout.write("Checking system security features\n\n")
-
-ssh = ssh()
-ssh.root_logins()
-ssh.privilege_separation()
-ssh.strict_modes()
-ssh.permit_empty_passwords()
+sys.exit(1)
