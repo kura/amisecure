fcccd3fde14e9b35bc9427eadc37ca8d9fbd7090
Kura <kura@deviling.net>
2011-09-08 15:00:12 +0100

    Added a new test, clean up code. New ss

--- amisecure.py
+++ amisecure.py
@@ -30,22 +30,22 @@ config_checks = (
         ),
         "tests": (
             (
-                re.compile(r"[^.*]PermitRootLogin\s(?P<value>yes|no)", re.IGNORECASE),
+                re.compile(r"[^a-z]PermitRootLogin\s(?P<value>yes|no)", re.IGNORECASE),
                 ("equal_to", "no"), True,
                 "Permit root logins", "Always disable this option"
             ),
             (
-                re.compile(r"[^.*]UsePrivilegeSeparation\s(?P<value>yes|no)", re.IGNORECASE), 
+                re.compile(r"[^a-z]UsePrivilegeSeparation\s(?P<value>yes|no)", re.IGNORECASE), 
                 ("equal_to", "yes"), True,
                 "Use privilege separation", "Always enable this option"
             ),
             (
-                re.compile(r"[^.*]StrictModes\s(?P<value>yes|no)", re.IGNORECASE), 
+                re.compile(r"[^a-z]StrictModes\s(?P<value>yes|no)", re.IGNORECASE), 
                 ("equal_to", "yes"), True,
                 "Use strict modes", "Always enable this option"
             ),
             (
-                re.compile(r"[^.*]PermitEmptyPasswords\s(?P<value>yes|no)", re.IGNORECASE), 
+                re.compile(r"[^a-z]PermitEmptyPasswords\s(?P<value>yes|no)", re.IGNORECASE), 
                 ("equal_to", "no"), True,
                 "Permit empty passwords", "Always disable this option"
             ),
@@ -56,62 +56,68 @@ config_checks = (
         "content_function": "get_file_content",
         "check_function": "check_value",
         "files": (
+            "/etc/apache2/apache2.conf",
+            "/etc/apache2/mods-enabled/*",
             "/etc/apache2/httpd.conf",
             "/etc/apache2/ports.conf",
-            "/etc/apache2/apache2.conf",
             "/etc/apache2/conf.d/*",
             "/etc/apache2/sites-enabled/*",
         ),
         "tests": (
             (
-                re.compile(r"[^.*]Timeout\s(?P<value>[0-9]*)", re.IGNORECASE), 
+                re.compile(r"[^a-z]Timeout\s(?P<value>[0-9]*)", re.IGNORECASE), 
                 ("less_than", 6), True,
                 "Timeout", "Less than 5 seconds is good"
             ),
             (
-                re.compile(r"[^.*]KeepAliveTimeout\s(?P<value>[0-9]*)", re.IGNORECASE), 
+                re.compile(r"[^a-z]KeepAliveTimeout\s(?P<value>[0-9]*)", re.IGNORECASE), 
                 ("less_than", 4), True,
                 "Keep alive timeout", "Less than 3 seconds is good"
             ),
             (
-                re.compile(r"[^.*]ServerTokens\s(?P<value>Prod|ProductOnly|Major|Minor|Min|Minimal|OS|Full)", re.IGNORECASE), 
-                ("equal_to", ("prod", "productonly", "major")), True,
-                "Server tokens", "Prod, ProductOnly and Major are considered 'secure' but both are the same"
+                re.compile(r"[^a-z]ServerTokens\s(?P<value>Prod|Major|Minor|Minimal|OS|Full)", re.IGNORECASE), 
+                ("equal_to", ("prod", "major")), True,
+                "Server tokens", "Prod and Major are considered 'secure' but both are the same"
             ),
             (
-                re.compile(r"[^.*]ServerSignature\s(?P<value>on|off)", re.IGNORECASE),
+                re.compile(r"[^a-z]ServerSignature\s(?P<value>on|off)", re.IGNORECASE),
                 ("equal_to", "off"), True,
                 "Server signature", "Off is considered 'secure'"
             ),
             (
-                re.compile(r"[^.*]traceenable\s(?P<value>on|off)", re.IGNORECASE),
+                re.compile(r"[^a-z]traceenable\s(?P<value>on|off)", re.IGNORECASE),
                 ("equal_to", "off"), True,
                 "Trace Enable", "Always disable unless required"
             ),
             (
-                re.compile(r"[^.*]Options\s.*?(?P<value>Includes).*", re.IGNORECASE),
+                re.compile(r"[^a-z]Options\s.*?(?P<value>\+?Includes).*", re.IGNORECASE),
                 ("equal_to", ""), "Found",
                 "ServerSide includes", "Only enable when required"
             ),
                         (
-                re.compile(r"[^.*]Options\s.*?(?P<value>ExecCGI).*", re.IGNORECASE),
+                re.compile(r"[^a-z]Options\s.*?(?P<value>\+?ExecCGI).*", re.IGNORECASE),
                 ("equal_to", ""), "Found",
                 "CGI execution", "Only enable when if you're using CGI"
             ),
             (
-                re.compile(r"[^.*]Options\s.*?(?P<value>Indexes).*", re.IGNORECASE),
+                re.compile(r"[^a-z]Options\s.*?(?P<value>\+?Indexes).*", re.IGNORECASE),
                 ("equal_to", ""), "Found",
-                "Directory Listing", "Enabling this allows people to browse you web-filesystem"
+                "Directory Listing", "Enabling this allows people to browse your web-filesystem"
             ),
             (
-                re.compile(r"[^.*]ScriptAlias\s(?P<value>/cgi-bin/).*", re.IGNORECASE),
+                re.compile(r"[^a-z]ScriptAlias\s(?P<value>/cgi-bin/).*", re.IGNORECASE),
                 ("equal_to", ""), "Found",
                 "cgi-bin alias", "Controls cgi-bin aliasing"
             ),
             (
-                re.compile(r"[^.*]Alias\s(?P<value>/doc/).*", re.IGNORECASE),
+                re.compile(r"[^a-z]Alias\s(?P<value>/doc/).*", re.IGNORECASE),
                 ("equal_to", ""), "Found",
                 "Docs alias", "Controls Apache doc aliasing"
+            ),
+            (
+                re.compile(r"[^a-z]Alias\s(?P<value>/icons/).*", re.IGNORECASE),
+                ("equal_to", ""), "Found",
+                "Icons alias", "Controls Apache icon aliasing"
             ),
         ),
     },
@@ -126,7 +132,7 @@ config_checks = (
         ),
         "tests": (
             (
-                re.compile(r"[^.*]server_tokens\s(?P<value>on|off)", re.IGNORECASE), 
+                re.compile(r"[^a-z]server_tokens\s(?P<value>on|off)", re.IGNORECASE), 
                 ("equal_to", "off"), True,
                 "Server tokens", "Hides or shows nginx version information"
             ),
@@ -142,32 +148,32 @@ config_checks = (
         ),
         "tests": (
             (
-                re.compile(r"[^.*]expose_php\s=\s(?P<value>on|off)", re.IGNORECASE), 
+                re.compile(r"[^a-z]expose_php\s=\s(?P<value>on|off)", re.IGNORECASE), 
                 ("equal_to", "off"), True,
                 "Expose PHP", "Controls PHP exposing itself via HTTP headers etc"
             ),
             (
-                re.compile(r"[^.*]register_globals\s=\s(?P<value>on|off)", re.IGNORECASE), 
+                re.compile(r"[^a-z]register_globals\s=\s(?P<value>on|off)", re.IGNORECASE), 
                 ("equal_to", "off"), True,
                 "Register globals", "Controls whether GET, POST, etc variables are globally registered"
             ),
             (
-                re.compile(r"[^.*]display_errors\s=\s(?P<value>on|off)", re.IGNORECASE), 
+                re.compile(r"[^a-z]display_errors\s=\s(?P<value>on|off)", re.IGNORECASE), 
                 ("equal_to", "off"), True,
                 "Display errors", "Controls whether PHP prints errors"
             ),
             (
-                re.compile(r"[^.*]session.use_only_cookies\s=\s(?P<value>1|0)", re.IGNORECASE),
+                re.compile(r"[^a-z]session.use_only_cookies\s=\s(?P<value>1|0)", re.IGNORECASE),
                 ("equal_to", "1"), True,
                 "Use only cookies", "Prevents attacks involving passing session ids in URLs"
             ),
             (
-                re.compile(r"[^.*]session.cookie_httponly\s=\s(?P<value>1|0)", re.IGNORECASE),
+                re.compile(r"[^a-z]session.cookie_httponly\s=\s(?P<value>1|0)", re.IGNORECASE),
                 ("equal_to", "1"), True,
                 "HTTPOnly cookies", "Cookies set by the server can only be read by the client"
             ),
             (
-                re.compile(r"[^.*]session.use_trans_sid\s=\s(?P<value>1|0)", re.IGNORECASE),
+                re.compile(r"[^a-z]session.use_trans_sid\s=\s(?P<value>1|0)", re.IGNORECASE),
                 ("equal_to", "0"), True,
                 "Session trans SID", "Enables or disables URL-based session ids"
             ),
@@ -215,7 +221,7 @@ def equal_to(this, that):
     """Convert values to strings and check if they match"""
     if isinstance(that, (tuple)):
         for v in that:
-            if str(this).lower() == str(that).lower():
+            if str(this).lower() == str(v).lower():
                 return True
     if str(this).lower() == str(that).lower():
         return True
@@ -256,6 +262,9 @@ def check_value(regex, secure_value, display_value, message, additional, content
         else:
             colour = "red"
             value = value + " (not secure)"
+    elif secure_value == "":
+        colour = "green"
+        value = "Not found (secure)"
     else:
         colour = "yellow"
         value = "unknown"
@@ -282,8 +291,8 @@ def clean(content):
     for line in content.split("\n"):
         line = line.lstrip()
         line = re.sub(r"\s+", " ", line)
-        if not re.match(r"[^.*]#", line) and not re.match(r"#", line) \
-        and not re.match(r"[^.*];", line) and not re.match(r";", line):
+        if not re.match(r"[^a-z]#", line) and not re.match(r"#", line) \
+        and not re.match(r"[^a-z];", line) and not re.match(r";", line):
             stripped_content += line + "\n"
     return stripped_content
 
